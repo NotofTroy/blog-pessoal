1279b7d7aac5b44ba59906acfab84ab8b7790305
NotofTroy <helenacsfonseca@hotmail.com>
2024-08-06 00:11:00 -0300

    feat: add props and hooks to Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import Home from "./paginas/home/Home";
 import "./App.css";
+import { useState } from "react";
 
 
 function App() {
   return (
-   <Home />
+    <>
+      <h1>App</h1>
+      <Home
+        title="Componente Home"
+        description="Este é um componente React que recebe props."
+      />
+    </>
+
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
 import './Home.css';
+import { useState, useEffect } from "react";
 
-function Home(){
+interface minhaProps {
+    title: string
+    description: string
+}
+
+function Home(props:minhaProps){
+    const [completed, setCompleted] = useState(false);
+    const [tarefa, setTarefa] = useState("");
+
+    useEffect(() => {
+        if(completed) {
+            setTarefa("Parabéns! Você concluiu a tarefa!")
+        }
+    }, [completed])
+
     return (
-        <>
-            <h1 className="titulo">Home</h1>
+        <>            
+            <h1 className="titulo">{props.title}</h1>
+            <p>{props.description}</p>
+            <button onClick={()=> setCompleted(true)}>Concluir tarefa</button>
+            <h3>{tarefa}</h3>
+
             <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
         </>
     );
