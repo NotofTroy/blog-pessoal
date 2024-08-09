807abc826da837f9d6b718e8f64f225c2af0b4c1
NotofTroy <helenacsfonseca@hotmail.com>
2024-08-09 15:35:44 -0300

    feat: add user signup and signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import './App.css';
-import Home from './paginas/home/Home';
-import Login from './paginas/login/Login';
-import Navbar from './componentes/NavBar/NavBar'
+
+import Navbar from './componentes/NavBar/NavBar';
 import Footer from './componentes/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
+import Home from './paginas/home/Home';
+import { AuthProvider } from './contextos/AuthContext';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className='min-h-[80vh]'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
     </>
   );
 }
