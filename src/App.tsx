9431aa77274d05223596499f8b0969d677bcd04f
NotofTroy <helenacsfonseca@hotmail.com>
2024-08-12 15:32:22 -0300

    feat: add theme component and CRUD integration

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contextos/AuthContext';
+import ListaTemas from './componentes/Temas/listaTemas/ListaTemas';
+import FormularioTema from './componentes/Temas/formularioTema/FormularioTema';
+import DeletarTema from './componentes/Temas/deletarTema/DeletarTema';
+import ListaPostagens from './componentes/Postagens/listaPostagens/ListaPostagens';
 
 function App() {
   return (
@@ -20,6 +24,11 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
             </Routes>
           </div>
           <Footer />
